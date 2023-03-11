c0179eefc9b46c9ff127dfeb3a45331c0c2d42c0
Yoji Sekiya <yojisekiya@gmail.com>
2023-03-11 20:11:32 +0900

    stripe checkout session

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,27 @@
+class CheckoutController < ApplicationController
+  def create
+    product = Product.find(params[:id])
+    @session = Stripe::Checkout::Session.create(
+      {
+        line_items: [
+          {
+            price_data: {
+              currency: 'usd',
+              unit_amount: product.price,
+              product_data: {
+                name: product.name
+              }
+            },
+            quantity: 1
+          }
+        ],
+        mode: 'payment',
+        success_url: root_url,
+        cancel_url: root_url
+      }
+    )
+    respond_to do |format|
+      format.js
+    end
+  end
+end
