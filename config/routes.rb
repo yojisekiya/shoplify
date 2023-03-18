799f70c904a4d7f67e08c447e3aa3675e3c71840
Yoji Sekiya <yojisekiya@gmail.com>
2023-03-18 23:04:41 +0900

    prepare for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'products#index'
   resources :products
-  resources :checkout, only: [:create]
   post 'checkout/create', to: 'checkout#create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :webhooks, only: [:create]
 end
