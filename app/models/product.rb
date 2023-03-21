6a70df37855d836ecc07ce0bac5173b6ddfa16c0
Yoji Sekiya <yojisekiya@gmail.com>
2023-03-21 23:25:58 +0900

    add stripe product id to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,10 @@ class Product < ApplicationRecord
   def to_s
     name
   end
+
+  after_create do
+    product = Stripe::Product.create(name: name)
+    price = Stripe::Price.create(product: product, unit_amount: self.price, currency: "usd")
+    update(stripe_product_id: product.id)
+  end
 end
