08533b2c96159b20adcc6ec659374c6d6b78767f
Yoji Sekiya <yojisekiya@gmail.com>
2023-03-22 08:26:34 +0900

    fix error

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -6,7 +6,6 @@ class CheckoutController < ApplicationController
         customer: current_user.stripe_customer_id,
         line_items: [
           {
-            currency: 'usd',
             price: product.stripe_price_id,
             quantity: 1
           }
