96310c05169eb161732f58e9b48af051f49bbe57
Yoji Sekiya <yojisekiya@gmail.com>
2023-03-23 08:26:55 +0900

    modify to checkout all products in the cart

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,15 +1,9 @@
 class CheckoutController < ApplicationController
   def create
-    product = Product.find(params[:id])
     @session = Stripe::Checkout::Session.create(
       {
         customer: current_user.stripe_customer_id,
-        line_items: [
-          {
-            price: product.stripe_price_id,
-            quantity: 1
-          }
-        ],
+        line_items: @cart.collect {|item| item.to_builder.attributes!},
         mode: 'payment',
         success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
         cancel_url: cancel_url
