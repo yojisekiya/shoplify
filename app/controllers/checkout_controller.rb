66876126fdcfd4f74e9a543810ac64eec8d2d1f5
Yoji Sekiya <yojisekiya@gmail.com>
2023-03-27 08:56:46 +0900

    empty cart after success

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -15,10 +15,15 @@ class CheckoutController < ApplicationController
   end
 
   def success
-    @session_with_expand = Stripe::Checkout::Session.retrieve({id: params[:session_id], expand: ["line_items"]})
-    @session_with_expand.line_items.data.each do |line_item|
-      product = Product.find_by(stripe_product_id: line_item.price.product)
-      # product.increment!(:sales_count)
+    if params[:session_id].present?
+      session[:cart] = []
+      @session_with_expand = Stripe::Checkout::Session.retrieve({id: params[:session_id], expand: ["line_items"]})
+      @session_with_expand.line_items.data.each do |line_item|
+        product = Product.find_by(stripe_product_id: line_item.price.product)
+        # product.increment!(:sales_count)
+      end
+    else
+      redirect_to cancel_url, alert: "No info to display"
     end
   end
   
